1a64cf5ee81388adde4aead883997ffba344b513
ABing <aaabingbingbing@foxmail.com>
2023-09-14 09:33:10 +0800

    :rocket: auto deploy

--- scripts/cmt.js
+++ scripts/cmt.js
@@ -1,7 +1,7 @@
 /**
  * 自定义 commit 脚本（由于各个包管理器对应配置项不同故仅兼容 npm）
- * @description `npm run cmt` => `git add . && git commit -m ":rocket: auto deploy" && git push`
- * @description `npm run cmt -m={msg}` => `git add . && git commit -m ":rocket: {msg}" && git push`
+ * @description `npm run cmt` => `git pull && git add . && git commit -m ":rocket: auto deploy" && git push`
+ * @description `npm run cmt -m={msg}` => `git pull && git add . && git commit -m ":rocket: {msg}" && git push`
  */
 const { execSync } = require('child_process')
 
